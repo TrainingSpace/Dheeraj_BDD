8ff3299ce49a3ed2278932f1edb8001319549b5a
dheeraj93 <dheeraj.g.kumar@accenture.com>
2016-08-11 19:54:34 +0530

    initial commit

---
+++ src/test/java/com/Accenture/com/Dheeraj_BDD/AC001_SampleBanking_StepDefinition.java
@@ -0,0 +1,12 @@
+package com.Accenture.com.Dheeraj_BDD;
+
+import cucumber.api.java.en.Given;
+
+public class AC001_SampleBanking_StepDefinition {
+	@Given("^a user access the bank web app$")
+	public void a_user_access_the_bank_web_app() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+}
